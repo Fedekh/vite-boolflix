c3d97728e89ccfa99a0d10520d4b3f0b82e0b192
Fedekh <federicocet@gmail.com>
2023-04-19 17:16:01 +0200

    TRY TO IMPLEMENTATE FLAG OR SIGLE OF NATION

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,7 +7,8 @@ export default {
 
     data() {
         return {
-            store
+            store,
+            flags: ["fr", "ge", "it", "ja", "tk", "en", "us"]
         }
     }
 }
@@ -17,8 +18,36 @@ export default {
 <template>
     <section class="film container mt-3">
         <h2 class="mb-4 text-white">FILM</h2>
-        <div class="row row-cols-lg-5 row-cols-md-3 row-cols-sm-2 row-cols-xs-1">
+        <div class="row row-cols-lg-5 mx-1 row-cols-md-3 row-cols-sm-2 row-cols-xs-1">
             <ul v-for="elemento in store.filmsArray">
+                <img :src="`${store.imgPath}${elemento.poster_path}`" alt="">
+                <h3 v-if="!elemento.poster_path">Spiacenti, immagine non disponibile</h3>
+                <li>Titolo: {{ elemento.title }}</li>
+                <li>Titolo originale: {{ elemento.original_title }}</li>
+                <li>Lingua:
+                    <span v-if="flags.includes((elemento.original_language).png)"> <img
+                            src="../assets/img/${elemento.original_language}.png" alt=""></span>
+                    <span v-else>
+
+                        {{ elemento.original_language }}
+                    </span>
+                </li>
+                <li>Voto: {{ elemento.vote_average }}</li>
+                <li>Stelle:
+                    <span v-for="i in store.stars">
+                        <i
+                            :class="[(i <= Math.floor(elemento.vote_average / 2)) ? 'fa-solid fa-star' : 'fa-regular fa-star']"></i>
+                    </span>
+                    <!-- <img src="../assets/img/.png" alt=""> -->
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="film container mt-3">
+        <h2 class="mb-4 text-white">SERIE TV</h2>
+        <div class="row row-cols-lg-5 mx-1 row-cols-md-3 row-cols-sm-2 row-cols-xs-1">
+            <ul v-for="elemento in store.tvArray">
                 <img :src="`${store.imgPath}${elemento.poster_path}`" alt="">
                 <h3 v-if="!elemento.poster_path">Spiacenti, immagine non disponibile</h3>
                 <li>Titolo: {{ elemento.title }}</li>
@@ -31,23 +60,6 @@ export default {
                             :class="[(i <= Math.floor(elemento.vote_average / 2)) ? 'fa-solid fa-star' : 'fa-regular fa-star']"></i>
                     </span>
                 </li>
-
-                <!-- <i class="fa-solid fa-star"></i> piena -->
-                <!-- <i class="fa-regular fa-star"></i> vuota -->
-            </ul>
-        </div>
-    </section>
-
-    <section class="serie container">
-        <h2 class="mb-4 text-white">SERIE TV</h2>
-        <div class="row row-cols-lg-5 row-cols-md-3 row-cols-sm-2 row-cols-xs-1">
-            <ul v-for="elemento in store.tvArray">
-                <img :src="`${store.imgPath}${elemento.poster_path}`" alt="">
-
-                <li>{{ elemento.title }}</li>
-                <li>{{ elemento.original_title }}</li>
-                <li>{{ elemento.original_language }}</li>
-                <li>{{ elemento.vote_average }}</li>
             </ul>
         </div>
     </section>
@@ -60,13 +72,18 @@ export default {
 @use "../style/partials/variables.scss" as *;
 
 .container {
-    cursor: pointer;
 
     .row {
         border: 1px solid red;
         color: white;
-        .fa-star{
-            color: yellow;
+
+        ul {
+            cursor: pointer;
+            border: 1px solid green;
+
+            .fa-star {
+                color: yellow;
+            }
         }
     }
 }
